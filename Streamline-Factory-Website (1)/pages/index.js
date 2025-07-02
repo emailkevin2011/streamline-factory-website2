db7c42f6bd7ff46784df793761404c08919215d8
emailkevin2011 <emailkevin2011@gmail.com>
2025-07-02 13:31:44 -0500

    Add files via upload

---
+++ Streamline-Factory-Website (1)/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Streamline Factory</h1>
+      <p>This is your Next.js homepage!</p>
+    </div>
+  );
+}
